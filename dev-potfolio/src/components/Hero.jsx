72448b50d05b2e1a922cb991e5e17d559bcef302
jamadrac <rabbikindalo001@@gmale.com>
2023-07-09 16:25:01 +0200

    dr

--- dev-potfolio/src/components/Hero.jsx
+++ dev-potfolio/src/components/Hero.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import hero from '../assets/images/hero.png'
 const Hero = () => {
-  return <div>Hero</div>;
+  return (<section id="home"
+  className="mi-h-screen fix py-10 md:flex-row flex-col items-center">
+    <div>
+      <img src={hero} alt="" className="md:w-11/12 h-full object-cover"/>
+    </div>
+    <div className="flex-1 flex items-center justify-center">
+      
+    </div>
+  </section>)
 };
-
+  
 export default Hero;
--- dev-potfolio/src/components/Navbar.jsx
+++ dev-potfolio/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 
 const Navbar = () => {
-  const [sticky, setSticky] = useState(true);
+  const [sticky, setSticky] = useState(false);
+  const [open, setOpen] = useState(false);
   const menuLinks = [
     { name: "HOME", link: "#home" },
     { name: "ABOUT", link: "#about" },
@@ -12,7 +13,7 @@ const Navbar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrollTop = window.pageYOffset;
+      const scrollTop = window.scrollY;
       setSticky(scrollTop > 0);
     };
 
@@ -22,17 +23,36 @@ const Navbar = () => {
     };
   }, []);
 
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
+
   return (
-    <>
-      <nav className={`fixed w-full left-0 top-0 z-[999] ${sticky ? 'bg-white text-gray-900' : 'text-white'}`}>
-        <div className="flex items-center justify-between">
-          <div className="mx-7">
-            <h4 className="text-4xl uppercase font-bold">
-              Ra<span className="text-cyan-600">BB</span>I
-            </h4>
-          </div>
-          <div className="text-gray-900 md:block hidden px-7 py-2 font-medium bg-white rounded-bl-full">
-            <ul className="flex items-center gap-1 py-2 text-lg">
+    <nav className={`fixed w-full left-0 top-0 z-[999] ${sticky ? "bg-white/60 text-gray-900" : "text-white"}`}>
+      <div className="flex items-center justify-between">
+        <div className="mx-7">
+          <h4 className="text-4xl uppercase font-bold">
+            Ra<span className="text-cyan-600">BB</span>I
+          </h4>
+        </div>
+        <div className={`${sticky ? "md:bg-white/0" : "bg-white"} text-gray-900 md:block hidden px-7 py-2 font-medium rounded-bl-full`}>
+          <ul className="flex items-center gap-1 py-2 text-lg">
+            {menuLinks?.map((menu, i) => (
+              <li key={i} className="px-6 hover:text-cyan-600">
+                <a href={menu?.link}>{menu?.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div
+          className={`z-[999] ${open ? "text-gray-900" : "text-gray-100"} text-3xl md:hidden`}
+          onClick={toggleMenu}
+        >
+          <ion-icon name="menu"></ion-icon>
+        </div>
+        {open && (
+          <div className="md:hidden text-gray-900 absolute w-2/3 h-screen px-7 py-2 font-medium bg-white top-0 right-0">
+            <ul className="flex flex-col justify-center h-full gap-10 py-2 text-lg">
               {menuLinks?.map((menu, i) => (
                 <li key={i} className="px-6 hover:text-cyan-600">
                   <a href={menu?.link}>{menu?.name}</a>
@@ -40,9 +60,9 @@ const Navbar = () => {
               ))}
             </ul>
           </div>
-        </div>
-      </nav>
-    </>
+        )}
+      </div>
+    </nav>
   );
 };
 
