3f01b3f070ee575a5b46eafdb117d217d1ac25c4
jamadrac <rabbikindalo001@@gmale.com>
2023-07-10 14:16:36 +0200

    ,,,,,

--- dev-potfolio/src/components/Hireme.jsx
+++ dev-potfolio/src/components/Hireme.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Hireme = () => {
-  return <div>Hireme</div>;
+  return(
+    <section id="hireme" className="py-10 px-3 text-white">
+      <div className="text-center">
+        <h3 className="text-4xl font-semibold">
+          hire <span className="text-cyan-600">Me</span>
+        </h3>
+      </div>
+      <div className="bg-gray-700 relative px-8 rounded-2xl py-5 lg:max-w-4xl mx-auto min-h-[24rem] mt-24 flex gap-6 lg:flex-row flex-col-reverse items-center"></div>
+      <div>
+        <h2 className="text-2xl font-semibold">Do you have some dseignes that you want to come to reality ?</h2>
+        <p className="max-w-lg tex-sm mt-4 tet-gray-200 leading-6"></p>
+        </div>
+      <img src="" alt="" /> 
+    </section>
+    )
 };
 
-export default Hireme;
+export default Hireme;  
