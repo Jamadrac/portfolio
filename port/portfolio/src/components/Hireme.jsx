3e62b96c60cc95ba34fbe009b3c2787db1c2b20c
jamadrac <rabbikindalo001@@gmale.com>
2023-07-13 01:23:27 +0200

    n

--- port/portfolio/src/components/Hireme.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Hireme = () => {
-  return (
-    <div>Hireme</div>
-  )
-}
--- port/portfolio/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.jsx, js'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
